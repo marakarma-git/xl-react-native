4f72890bc395f85b1315937a6cc8d3bb6fcf0e62
Risardi <risardi123@gmail.com>
2021-01-06 15:01:08 +0700

    Add: Splash screen, apps icon, and rename apps

--- android/app/src/main/java/com/dcp_mobile_v4/MainActivity.java
+++ android/app/src/main/java/com/dcp_mobile_v4/MainActivity.java
@@ -1,9 +1,14 @@
 package com.dcp_mobile_v4;
 
 import com.facebook.react.ReactActivity;
-
+import android.os.Bundle; // here
+import org.devio.rn.splashscreen.SplashScreen; // here
 public class MainActivity extends ReactActivity {
-
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this,true);  // here
+    super.onCreate(savedInstanceState);
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
--- src/pages/auth.page.js
+++ src/pages/auth.page.js
@@ -4,9 +4,11 @@ import {
   Text
 } from 'react-native'
 import {useSelector} from 'react-redux'
+import SplashScreen from 'react-native-splash-screen'
 const Auth = ({navigation}) =>{
   const dummy_action = useSelector(state=> state.dummy_reducer)
   useEffect(()=>{
+    SplashScreen.hide()
     navigation.replace("Login")
   },[navigation])
   return(
