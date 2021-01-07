2c393f8bc5bec654249fad22247084ba18e752bc
Risardi <risardi123@gmail.com>
2021-01-07 10:52:06 +0700

    Fix: auth logout when checkToken.data isEmpty

--- src/pages/auth.page.js
+++ src/pages/auth.page.js
@@ -1,18 +1,21 @@
 import React, {useEffect} from 'react';
 import {View, ActivityIndicator} from 'react-native';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import SplashScreen from 'react-native-splash-screen';
 import lod from 'lodash';
+import reduxString from '../redux/reduxString';
 const Auth = ({navigation}) => {
+  const dispatch = useDispatch();
   const checkToken = useSelector((state) => state.auth_reducer);
   useEffect(() => {
     SplashScreen.hide();
     if (lod.isEmpty(checkToken.data)) {
+      dispatch(reduxString.AUTH_LOGOUT);
       navigation.replace('Login');
     } else {
       navigation.replace('Home');
     }
-  }, [checkToken.data, navigation]);
+  }, [checkToken.data, dispatch, navigation]);
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <ActivityIndicator size={'large'} color={'black'} />
