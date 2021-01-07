ffd66f54ea2c393cc596edc204ba2b983be41752
Risardi <risardi123@gmail.com>
2021-01-07 10:59:33 +0700

    Fix: dispatch authLogout

--- src/pages/auth.page.js
+++ src/pages/auth.page.js
@@ -3,14 +3,14 @@ import {View, ActivityIndicator} from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
 import SplashScreen from 'react-native-splash-screen';
 import lod from 'lodash';
-import reduxString from '../redux/reduxString';
+import {authLogout} from '../redux/action/auth_action';
 const Auth = ({navigation}) => {
   const dispatch = useDispatch();
   const checkToken = useSelector((state) => state.auth_reducer);
   useEffect(() => {
     SplashScreen.hide();
     if (lod.isEmpty(checkToken.data)) {
-      dispatch(reduxString.AUTH_LOGOUT);
+      dispatch(authLogout());
       navigation.replace('Login');
     } else {
       navigation.replace('Home');
