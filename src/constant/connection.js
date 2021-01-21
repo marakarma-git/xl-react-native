863cd098cd34bdaa805755af16e1fe15f2d23a14
Arona Nur Tetulis <arona.nur.tetulis@gmail.com>
2021-01-21 13:18:43 +0700

    Modified dataset_base_url

--- src/constant/connection.js
+++ src/constant/connection.js
@@ -4,7 +4,7 @@ const headerAuth = 'Basic eGwtZGNwLXNlY3VyaXR5OnhsLWRjcC1zZWN1cml0eS1zZWNyZXQ=';
 const super_base_url = 'http://18.141.55.24';
 const base_url = `${super_base_url}/api`;
 const dashboard_base_url = 'http://18.141.55.24/api/dcp/dashboard';
-const dataset_base_url = 'http://18.141.55.24:18084/dashboard/v2';
+const dataset_base_url = 'http://18.141.55.24/api/dcp/dashboard/v2';
 
 export {
   headerAuth,
