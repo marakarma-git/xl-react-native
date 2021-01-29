0c53444d810392a545e0716de0c9e027493a0945
Arona Nur Tetulis <arona.nur.tetulis@gmail.com>
2021-01-29 15:34:50 +0700

    Fixed app js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { headerAuth } from './constant/connection';
 import {persistStore, persistReducer} from 'redux-persist';
 const thunk = require('redux-thunk').default;
 import {validateTokenMiddleware} from './redux/middleware/index';
@@ -8,6 +9,7 @@ import {PersistGate} from 'redux-persist/integration/react';
 import {applyMiddleware, createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import { encryptTransform } from 'redux-persist-transform-encrypt';
 import Route from './pages/route';
 import RootReducers from './redux/reducer';
 
