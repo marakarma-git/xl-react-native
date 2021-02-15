6739115ffaa5547a239689ef83f907bd3bf2a581
Kamal <imkamalh@gmail.com>
2021-02-15 11:42:31 +0700

    revert subscription page

--- src/pages/Home/subscription.page.js
+++ src/pages/Home/subscription.page.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useCallback} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {Text, View} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
@@ -25,17 +25,12 @@ import {
 import ModalMenuPicker from '../../components/modal/ModalMenuPicker';
 import AppliedFilter from '../../components/subscription/appliedFilter';
 import dayjs from 'dayjs';
-import {ScrollView} from 'react-native-gesture-handler';
-import {Dimensions} from 'react-native';
-import Orientation from '../../helpers/orientation';
 
 const Subscription = () => {
   const dispatch = useDispatch();
   const navigation = useNavigation();
   const [firstRender, setFirstRender] = useState(true);
   const [showMenu, setShowMenu] = useState(false);
-  const [orientation, setOrientation] = useState('potrait');
-
   const {
     array_filter,
     loading_array_filter,
@@ -52,11 +47,8 @@ const Subscription = () => {
     current_total_page,
     current_header_sort,
   } = useSelector((state) => state.get_sim_inventory_reducer);
-  const {imageBase64} = useSelector((state) => state.enterprise_reducer);
-
   useEffect(() => {
     const timer = setTimeout(() => {
-      detectOrientation();
       if (!firstRender) {
         dispatch(
           callSimInventory({
@@ -74,116 +66,99 @@ const Subscription = () => {
   useEffect(() => {
     dispatch(generateArrayFilterParams());
   }, [array_filter]);
-
-  const detectOrientation = useCallback(() => {
-    if (Orientation.getHeight() <= Orientation.getWidth()) {
-      setOrientation('landscape');
-    }
-    Dimensions.addEventListener('change', () => {
-      setOrientation(Orientation.isPortrait() ? 'potrait' : 'landscape');
-    });
-  }, [Dimensions]);
-
-  const Wrapper = ({children, condition, wrapper}) =>
-    condition ? wrapper(children) : children;
-
   return (
-    <HeaderContainer headerTitle={`Subscription ${orientation}`} companyLogo={imageBase64}>
+    <HeaderContainer headerTitle={'Subscription'}>
       <View style={subscriptionStyle.containerBackground}>
-        <Wrapper
-          condition={orientation === 'landscape'}
-          wrapper={(children) => <ScrollView>{children}</ScrollView>}>
-          <OverlayBackground />
-          <SearchHeader
-            value={searchText}
-            onChangeText={(e) => dispatch(updateDataSearchText(e))}
-            showMenu={showMenu}
-            onClickColumn={() => setShowMenu((state) => !state)}
-            loading={loading_array_filter || loading}
-          />
-          <AppliedFilter
-            data={array_filter}
-            onDelete={(e) => {
-              const {formId, typeInput} = e || {};
-              dispatch(
-                setSomethingToFilter([
-                  {
-                    formId: formId,
-                    needs: `OnChange${typeInput}`,
-                    value:
-                      typeInput === 'DateTimePicker'
-                        ? dayjs().toDate()
-                        : typeInput === 'DropDown'
-                        ? {}
-                        : '',
-                    selectedValue: {},
-                    isSelected: false,
-                  },
-                ]),
-              );
-            }}
-          />
-          <FilterActionLabel filtered={totalFiltered} />
-          <Table
-            selectedHeaderOrderSort={current_header_sort}
-            dataHeader={array_filter}
-            dataTable={data_sim_inventory_table}
-            loading={loading}
-            onPressHeader={(e) => {
-              const {dataSort, item} = e || {};
-              const {sortBy, formId: currentFormId} = dataSort || {};
-              const {formId, api_id} = item || {};
-              const getSort = () => {
-                if (formId !== currentFormId) {
+        <OverlayBackground />
+        <SearchHeader
+          value={searchText}
+          onChangeText={(e) => dispatch(updateDataSearchText(e))}
+          showMenu={showMenu}
+          onClickColumn={() => setShowMenu((state) => !state)}
+          loading={loading_array_filter || loading}
+        />
+        <AppliedFilter
+          data={array_filter}
+          onDelete={(e) => {
+            const {formId, typeInput} = e || {};
+            dispatch(
+              setSomethingToFilter([
+                {
+                  formId: formId,
+                  needs: `OnChange${typeInput}`,
+                  value:
+                    typeInput === 'DateTimePicker'
+                      ? dayjs().toDate()
+                      : typeInput === 'DropDown'
+                      ? {}
+                      : '',
+                  selectedValue: {},
+                  isSelected: false,
+                },
+              ]),
+            );
+          }}
+        />
+        <FilterActionLabel filtered={totalFiltered} />
+        <Table
+          selectedHeaderOrderSort={current_header_sort}
+          dataHeader={array_filter}
+          dataTable={data_sim_inventory_table}
+          loading={loading}
+          onPressHeader={(e) => {
+            const {dataSort, item} = e || {};
+            const {sortBy, formId: currentFormId} = dataSort || {};
+            const {formId, api_id} = item || {};
+            const getSort = () => {
+              if (formId !== currentFormId) {
+                return 'ASC';
+              }
+              if (formId === currentFormId) {
+                if (sortBy === '') {
                   return 'ASC';
                 }
-                if (formId === currentFormId) {
-                  if (sortBy === '') {
-                    return 'ASC';
-                  }
-                  if (sortBy === 'ASC') {
-                    return 'DESC';
-                  }
-                  if (sortBy === 'DESC') {
-                    return '';
-                  }
+                if (sortBy === 'ASC') {
+                  return 'DESC';
                 }
-              };
-              if (getSort()) {
-                dispatch(
-                  callSimInventory({
-                    page_value: 0,
-                    selectedHeaderSort: {
-                      formId: formId,
-                      orderBy: api_id,
-                      sortBy: getSort(),
-                    },
-                  }),
-                );
-              } else {
-                dispatch(
-                  callSimInventory({
-                    page_value: 0,
-                  }),
-                );
+                if (sortBy === 'DESC') {
+                  return '';
+                }
               }
-            }}
-            onPressCheckHeader={({selectedValue}) => {
-              const {value} = selectedValue || {};
-              switch (value) {
-                case 'select_all_local':
-                  return dispatch(changeCheckSimInventoryAllTrue());
-                case 'deselect_all_local':
-                  return dispatch(changeCheckSimInventoryAllFalse());
-                default:
-                  return null;
-              }
-            }}
-            onPressCheckCell={({index}) =>
-              dispatch(changeCheckSimInventory(index))
+            };
+            if (getSort()) {
+              dispatch(
+                callSimInventory({
+                  page_value: 0,
+                  selectedHeaderSort: {
+                    formId: formId,
+                    orderBy: api_id,
+                    sortBy: getSort(),
+                  },
+                }),
+              );
+            } else {
+              dispatch(
+                callSimInventory({
+                  page_value: 0,
+                }),
+              );
             }
-          />
-        </Wrapper>
+          }}
+          onPressCheckHeader={({selectedValue}) => {
+            const {value} = selectedValue || {};
+            switch (value) {
+              case 'select_all_local':
+                return dispatch(changeCheckSimInventoryAllTrue());
+              case 'deselect_all_local':
+                return dispatch(changeCheckSimInventoryAllFalse());
+              default:
+                return null;
+            }
+          }}
+          onPressCheckCell={({index}) =>
+            dispatch(changeCheckSimInventory(index))
+          }
+        />
         <TableFooter
           loading={loading}
           totalPage={current_total_page}
