854530e856103d28d4af3b2101bcf118615cc24c
Kamal <imkamalh@gmail.com>
2021-03-02 10:43:04 +0700

    init ssl pinning

--- src/constant/connection.js
+++ src/constant/connection.js
@@ -1,7 +1,7 @@
 // http://172.30.251.160/api
 // http://52.237.113.189/api'
 const headerAuth = 'Basic eGwtZGNwLXNlY3VyaXR5OnhsLWRjcC1zZWN1cml0eS1zZWNyZXQ=';
-const super_base_url = 'http://18.141.55.24';
+const super_base_url = 'http://dcp4.adlsandbox.com';
 const base_url = `${super_base_url}/api`;
 
 export {headerAuth, super_base_url, base_url};
