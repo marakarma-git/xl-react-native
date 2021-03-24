fe4532069d99358593bdc408c240026079871104
Kamal <imkamalh@gmail.com>
2021-03-24 11:11:20 +0700

    add etc props

--- src/components/global/text.js
+++ src/components/global/text.js
@@ -30,7 +30,13 @@ const Text = (props) => {
     }
   };
 
-  return <MyText style={[props.style, fontWeight()]}>{props.children}</MyText>;
+  return (
+    <MyText
+      style={[props.style, fontWeight()]}
+      {...props}>
+      {props.children}
+    </MyText>
+  );
 };
 
 Text.propTypes = {
