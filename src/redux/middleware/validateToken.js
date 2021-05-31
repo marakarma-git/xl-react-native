724ad9f037b0a16ee637c5b3e77b2a8a1831f4a6
--replace <arona.nur.tetulis@gmail.com>
2021-05-31 10:03:47 +0700

    Edit validateToken middleware

--- src/redux/middleware/validateToken.js
+++ src/redux/middleware/validateToken.js
@@ -4,12 +4,17 @@ import {ToastAndroid} from 'react-native';
 const validateTokenMiddleware = (store) => (next) => (action) => {
 
   try {
+    console.log(action.payload)
     if (action.payload) {
       if (action.payload.error) {
         if (action.payload.error === 'invalid_token') {
           ToastAndroid.show('Token Expired', ToastAndroid.LONG);
           store.dispatch(authLogout());
           throw new Error('Token Expired');
+        }else if(action.payload.error === 'unauthorized'){
+          ToastAndroid.show('Token Invalid', ToastAndroid.LONG);
+          store.dispatch(authLogout());
+          throw new Error('Token Invalid');
         }
       }
     }
