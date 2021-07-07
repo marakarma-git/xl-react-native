f1fc4084e1e2006468af22dfd7c94f6f0c97badc
Arona Nur Tetulis <arona.nur.tetulis@gmail.com>
2021-07-07 10:09:44 +0700

    new Menu

---
+++ src/pages/Home/geoDistributionFilter.page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Text} from '../../components';
+
+const GeoDistributionFilterPage = () => {
+    return(
+        <View>
+            <Text>GeoDistributionFilterPage</Text>
+        </View>
+    );
+}
+
+export default GeoDistributionFilterPage;
---
+++ src/pages/Home/simProductivityFilter.page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Text} from '../../components';
+
+const SimProductivityFilterPage = () => {
+    return(
+        <View>
+            <Text>SimProductivityFilterPage</Text>
+        </View>
+    );
+}
+
+export default SimProductivityFilterPage;
