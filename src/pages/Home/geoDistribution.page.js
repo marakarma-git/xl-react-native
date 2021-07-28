975d80b6da51dbcf16c1a46afbcb0c81a244e574
Risardi <risardi123@gmail.com>
2021-07-28 16:21:52 +0700

    Remove console.log (SourceTree Running)

--- src/pages/Home/geoDistribution.page.js
+++ src/pages/Home/geoDistribution.page.js
@@ -87,7 +87,6 @@ const GeoDistributionPage = () => {
               provider={PROVIDER_GOOGLE}
               style={{flex: 1, marginTop: 12}}
               onRegionChange={(region) => {
-                console.log('test push');
                 const calculateLevel =
                   Math.round(
                     Math.log(360 / region.longitudeDelta) / Math.LN2,
