88d930d34601fd1303d7c81cfab336f91f9aa28a
Risardi <risardi123@gmail.com>
2021-09-03 17:22:59 +0700

    Run Eslint on file ModalConfirmation

--- src/components/modal/ModalConfirmation.js
+++ src/components/modal/ModalConfirmation.js
@@ -3,59 +3,74 @@ import {
   Modal,
   TouchableOpacity,
   View,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
 } from 'react-native';
 import {inputHybridStyle} from '../../style';
 import {Text} from '../index';
 import PropTypes from 'prop-types';
 
 const ModalConfirmation = (props) => {
-
-    return(
-    <Modal animationType="slide" 
-        transparent 
-        visible={props.showModal} 
-        onRequestClose={props.closeModal}>
-        <View style={inputHybridStyle.modalBackdrop} />
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <KeyboardAvoidingView style={[inputHybridStyle.multiSessionModal, { borderRadius: 5, width: '85%' }]}>
-                <View style={[inputHybridStyle.tcTitleContainer, { borderTopRightRadius: 5, borderTopLeftRadius: 5 }]}>
-                    <Text style={inputHybridStyle.modalTitleText, { color: 'white' }}>{props.title}</Text>
-                </View>
-                <View style={{ padding: 15 }}>
-                    <Text fontType="semi-bold" style={inputHybridStyle.tcText}>
-                      {props.description}
-                    </Text>
-                </View>
-                <View style={inputHybridStyle.modalFooter}>
-                    <View style={inputHybridStyle.buttonContainer}>
-                        <TouchableOpacity
-                            onPress={props.closeModal} 
-                            style={
-                              [
-                                inputHybridStyle.buttonCancel, 
-                                { borderRadius: 5, backgroundColor: '#CBC9C9', borderColor: '#CBC9C9' }
-                              ]
-                              }>
-                            <Text style={[inputHybridStyle.buttonText, { color: 'black' }]}>
-                                {props.cancelText}
-                            </Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                                disabled={props.loading}
-                                onPress={props.confirmAction}
-                                style={[inputHybridStyle.buttonNext, { borderRadius: 5, borderColor: 'none' }]}>
-                            <Text style={inputHybridStyle.buttonText}>
-                                {props.confirmText}
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </KeyboardAvoidingView>
-        </View>
+  return (
+    <Modal
+      animationType="slide"
+      transparent
+      visible={props.showModal}
+      onRequestClose={props.closeModal}>
+      <View style={inputHybridStyle.modalBackdrop} />
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <KeyboardAvoidingView
+          style={[
+            inputHybridStyle.multiSessionModal,
+            {borderRadius: 5, width: '85%'},
+          ]}>
+          <View
+            style={[
+              inputHybridStyle.tcTitleContainer,
+              {borderTopRightRadius: 5, borderTopLeftRadius: 5},
+            ]}>
+            <Text style={(inputHybridStyle.modalTitleText, {color: 'white'})}>
+              {props.title}
+            </Text>
+          </View>
+          <View style={{padding: 15}}>
+            <Text fontType="semi-bold" style={inputHybridStyle.tcText}>
+              {props.description}
+            </Text>
+          </View>
+          <View style={inputHybridStyle.modalFooter}>
+            <View style={inputHybridStyle.buttonContainer}>
+              <TouchableOpacity
+                onPress={props.closeModal}
+                style={[
+                  inputHybridStyle.buttonCancel,
+                  {
+                    borderRadius: 5,
+                    backgroundColor: '#CBC9C9',
+                    borderColor: '#CBC9C9',
+                  },
+                ]}>
+                <Text style={[inputHybridStyle.buttonText, {color: 'black'}]}>
+                  {props.cancelText}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                disabled={props.loading}
+                onPress={props.confirmAction}
+                style={[
+                  inputHybridStyle.buttonNext,
+                  {borderRadius: 5, borderColor: 'none'},
+                ]}>
+                <Text style={inputHybridStyle.buttonText}>
+                  {props.confirmText}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </KeyboardAvoidingView>
+      </View>
     </Modal>
-    );
-}
+  );
+};
 
 ModalConfirmation.propTypes = {
   showModal: PropTypes.bool,
@@ -65,8 +80,8 @@ ModalConfirmation.propTypes = {
   cancelText: PropTypes.string,
   confirmText: PropTypes.string,
   loading: PropTypes.bool,
-  confirmAction: PropTypes.func
-}
+  confirmAction: PropTypes.func,
+};
 
 ModalConfirmation.defaultProps = {
   showModal: false,
@@ -76,7 +91,7 @@ ModalConfirmation.defaultProps = {
   cancelText: 'Cancel',
   confirmText: 'Confirm',
   loading: false,
-  confirmAction: () => {}
-}
+  confirmAction: () => {},
+};
 
 export default ModalConfirmation;
